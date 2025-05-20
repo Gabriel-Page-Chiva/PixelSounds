e7883b23b9841f59bea3b53c8884339d1324fa77
R.Roman <luigiverdeyt@gmail.com>
2025-05-20 21:08:31 +0200

    JUNTADO JESU-CARDO

--- generarVideo.bat
+++ generarVideo.bat
@@ -1,10 +1,9 @@
-ffmpeg 
--framerate 25
--i fotogramas/bloque_%04d.png 
--i %1 
--c:v libx264 
--c:a aac 
--pix_fmt yuv420p 
--shortest 
+ffmpeg ^
+-framerate 25 ^
+-i fotogramas/frame_%%04d.png ^
+-i "%~1" ^
+-c:v libx264 ^
+-c:a aac ^
+-pix_fmt yuv420p ^
+-shortest ^
 salida.mp4
-
